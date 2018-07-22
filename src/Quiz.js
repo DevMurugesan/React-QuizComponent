8c63daa2a146bf267bb91569531327d28b2f5f5b
Murugesan <murugesamkce@gmail.com>
2018-07-22 23:16:57 +0530

    Final commit

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
@@ -9,8 +10,10 @@ constructor(props){
 }
 render(){
 	return (
+		
 		<div>
 		<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+		<QuizEnd/>
 		</div>
 		)
 }
